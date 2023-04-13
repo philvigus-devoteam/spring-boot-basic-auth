b4f5f7dbbfc7917cbfb5cefd6742f2e89cd40606
Phil Vigus <phil.vigus@devoteam.com>
2023-04-13 15:25:48 +0200

    Tidy up

--- src/main/java/com/philvigus/basicauthrestapi/config/ProjectConfig.java
+++ src/main/java/com/philvigus/basicauthrestapi/config/ProjectConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
